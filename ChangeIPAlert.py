5ceb79c7550dbbfbc494b4aac00c5f55245c0e47
Guillermo Sampallo <guille@gmail.com>
2023-11-13 20:42:37 -0300

    Update Cloudns

--- ChangeIPAlert.py
+++ ChangeIPAlert.py
@@ -11,6 +11,7 @@ class ChangeIPAlert():
         if new_ip:
             if not new_ip == self._old_ip:
                 print("The IP change from {} to {}".format(self._old_ip,new_ip))
+                self.update_dns()
                 self.send_alert(new_ip)
                 self.update_parameters(new_ip)
 
@@ -37,10 +38,14 @@ class ChangeIPAlert():
             json.dump(self.parameters, jsonFile)
         jsonFile.close()
 
+    def update_dns(self):
+        import requests
+        res = requests.get(self._url)
 
     def send_alert(self,ip):
         pb = Pushbullet(self._api_key)
         push = pb.push_note('Se actualizo la IP: {}'.format(ip), "Cambio de IP")
+        
 
 if __name__ == "__main__":
     alertas = ChangeIPAlert()
